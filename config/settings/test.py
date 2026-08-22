dd40fd9f5e54b8d405b5f63b4bd9a4c8df2d26ac
SomafGroup <support@somafgroup.com>
2026-08-22 07:09:02 +0200

    test: isolate account email and password infrastructure

--- config/settings/test.py
+++ config/settings/test.py
@@ -17,6 +17,8 @@ STORAGES = {
     "default": {"BACKEND": "django.core.files.storage.FileSystemStorage"},
     "staticfiles": {"BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage"},
 }
+EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
+PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
 CELERY_TASK_ALWAYS_EAGER = True
 CELERY_TASK_EAGER_PROPAGATES = True
 CELERY_BROKER_URL = "memory://"
