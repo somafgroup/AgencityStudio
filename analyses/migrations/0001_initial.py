d8bedd12290431c0a833da0a194ef4fc58df95c8
SomafGroup <support@somafgroup.com>
2026-08-25 17:16:13 +0200

    style: fix Plan 7 migration imports

--- analyses/migrations/0001_initial.py
+++ analyses/migrations/0001_initial.py
@@ -1,7 +1,8 @@
 # Generated for AgencityStudio Plan 7.
 
-import django.db.models.deletion
 import uuid
+
+import django.db.models.deletion
 from django.conf import settings
 from django.db import migrations, models
 
