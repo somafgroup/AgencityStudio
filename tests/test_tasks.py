c5e13adb6391e966328b325b2f7b425c11108019
SomafGroup <support@somafgroup.com>
2026-08-26 10:26:39 +0200

    test: cover diagnostic worker registration

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -1,4 +1,5 @@
 from common.tasks import TaskStatus, health_ping
+from config import celery_app
 
 
 def test_task_status_contract_is_stable():
@@ -11,3 +12,9 @@ def test_worker_health_task_executes_eagerly():
     result = health_ping.delay()
 
     assert result.get() == "pong"
+
+
+def test_diagnostic_task_module_is_registered_for_real_workers():
+    celery_app.loader.import_default_modules()
+
+    assert "analyses.execute_diagnostic_run" in celery_app.tasks
